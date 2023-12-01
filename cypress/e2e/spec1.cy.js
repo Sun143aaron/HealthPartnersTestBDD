a2dee91af26549a548a3d6d11dde8163c410d35b
Sun143aaron <sunil_baria@yahoo.com>
2023-12-01 15:16:26 +0000

    Automation Task

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -1,45 +1,9 @@
 /// <reference types="cypress-iframe" />
-import 'cypress-iframe';
 
 
 describe('test',()=> {
    
 it('e2e',()=>{
-        cy.visit('https://www.telegraph.co.uk/');
-        cy.url().should('contain','telegraph');
-    
-         cy.iframe("div[id='sp_message_container_784380'] iframe").then(($iframe) => {
-        // // Work with elements inside the iframe with the specified identifier
-         cy.wrap($iframe).find("button[title='Accept']").eq(1).click({force: true});
-
-      });
-      cy.get('span[class="e-navigation-primary-item__link-content"]').contains('Sport').click();
-      cy.title().then(title=>{
-        cy.log('The title is + ', title);
-
       })
-      cy.get('body').then(($div) => {
-
-        if($div.find('svg[class="martech-modal-component__close-icon"]').length > 0){
-          cy.get('svg[class="martech-modal-component__close-icon"]').click();
-  
-       } else{
-          cy.log('No Offer Alert');
-       }
-      })
-      cy.wait(2000)
-      cy.get('.accordion__trigger').click({force: true})
-      cy.get('.e-navigation-secondary-item__link-text').eq(4).click({force: true});
-
-      cy.title().then(title=>{
-        cy.log('The title is + ', title);
-
-      })
-      cy.title().should('include',"Women's Sport");
-
-      //.hero-asset__title
-
-
-})
 
 })
